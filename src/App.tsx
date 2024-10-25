2bca9a5a339d35a712b258a4ede1983b13dbc2d0
waszuch <140809966+waszuch@users.noreply.github.com>
2024-10-25 13:16:19 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
